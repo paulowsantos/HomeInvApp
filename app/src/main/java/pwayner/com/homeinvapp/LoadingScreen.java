4db7ba8db1b314195b3c7edd53a5876690319c3c
paulowsantos <paulowsantos@gmail.com>
2019-07-15 23:11:41 -0700

    Loading Screen created

---
+++ app/src/main/java/pwayner/com/homeinvapp/LoadingScreen.java
@@ -0,0 +1,32 @@
+package pwayner.com.homeinvapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        Thread mThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mThread.start();
+
+    }
+}
